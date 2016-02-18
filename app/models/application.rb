22acb126d33b3b58842cd0ef420146a2b207d6a0
William Ross <will@spanner.org>
2016-02-18 08:38:37 +0000

    do not preload applications: silly

--- app/models/application.rb
+++ app/models/application.rb
@@ -7,28 +7,19 @@ class Application
 
   class << self
 
-    def preload
-      @applications ||= self.all.fetch
-      @applications
-    end
-
-    def find(id)
-      preload.find{ |a| a.id.to_i == id.to_i }
-    end
-
     def in_round(round_id)
-      preload.select{ |a| a.round_id.to_i == round_id.to_i }
+      where(round_id: round_id)
     end
 
     def for_selection(round_id)
-      applications = round_id ? in_round(round_id) : preload
+      applications = round_id ? in_round(round_id) : all
       applications.map{|a| [a.serial_and_name, a.id] }
     end
   end
-  
+
   def serial_and_name
     serial_number = "##{serial}: " if serial?
     [serial_number, name, research_title].map(&:presence).compact.join(' ')
   end
-  
+
 end
