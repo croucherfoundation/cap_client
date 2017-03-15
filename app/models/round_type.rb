aa31eda9b4b027d2753df1d4cc0e71bf925bc850
will-r <will@spanner.org>
2017-03-15 15:57:45 +0000

    round types for selection

--- app/models/round_type.rb
+++ app/models/round_type.rb
@@ -1,7 +1,17 @@
 class RoundType
   include Her::JsonApi::Model
-
   use_api CAP
   collection_path "/api/round_types"
+
+  def self.new_with_defaults(attributes={})
+    RoundType.new({
+      name: "",
+      code: ""
+    }.merge(attributes))
+  end
+
+  def self.for_selection
+    RoundType.all.sort_by(&:name).map{|rt| [rt.name, rt.id] }
+  end
 
 end
