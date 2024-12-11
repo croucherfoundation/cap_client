1a4d6aeda44641f8ace13950750fa68f4658b64e
Thi Ha <thiha.dve@gmail.com>
2024-12-11 13:50:51 +0900

    partner dashboard add

--- app/models/nominator.rb
+++ app/models/nominator.rb
@@ -10,5 +10,26 @@ class Nominator
     def by_user_uid(user_uid)
       where(user_uid: user_uid)
     end
+
+    def new_with_defaults(attributes={})
+      nominator = Nominator.new({
+        user_uid: nil,
+        institution_code: "",
+        user_attributes: {
+          title: "", 
+          family_name: "", 
+          given_name: "", 
+          email: "", 
+          phone: "",
+          defer_confirmation: true
+    }
+
+      }.merge(attributes))
+      nominator
+    end
+  end
+
+  def invite
+    self.class.get("/api/nominators/#{id}/invite")
   end
 end
